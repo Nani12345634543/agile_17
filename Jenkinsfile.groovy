f7dbe5c71f6777b17ae60bc26dccb425c164801d
Nani12345634543 <nani20242002@gmail.com>
2026-09-17 10:42:05 +0530

    commit4

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,20 +1,25 @@
 pipeline {
     agent any
     parameters {
-        choice(name: 'ENVIRONMENT', choices: ['dev', 'staging', 'prod'], description: 'Select the deploy')
-        stages {
-            stage('Checkout') {
-                steps {
+        choice(
+            name: 'ENVIRONMENT', 
+            choices: ['dev', 'staging', 'prod'], 
+            description: 'Select the deployment environment' // Fixed here
+        )
+    }
+    stages {
+        stage('Checkout') {
+            steps {
                 git branch: 'main', url: 'https://github.com/Nani12345634543/agile_17.git'
-                }
             }
-            stage('Show Parameter') {
-                steps {
+        }
+        stage('Show Parameter') {
+            steps {
                 echo "Selected environment: ${params.ENVIRONMENT}"
-                }
             }
-            stage('Build for Environment') {
-                steps {
+        }
+        stage('Build for Environment') {
+            steps {
                 echo "Building the application for the ${params.ENVIRONMENT} environment..."
             }
         }
